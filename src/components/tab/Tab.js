9d227e85c230dc0eab567f8a8d0655a48ac0b539
Shivam <shivam.gup82@gmail.com>
2022-05-01 08:35:39 +0530

    Added links

--- src/components/tab/Tab.js
+++ src/components/tab/Tab.js
@@ -5,7 +5,7 @@ import VideocamIcon from "@mui/icons-material/Videocam";
 export default function Tab({ type }) {
   return (
     <div className='flex flex-col mx-4'>
-      <div className='flex  items-center justify-center hover:cursor-pointer hover:bg-white hover:text-black text-white bg-black border-2 border-white w-[30px] p-6 m-2'>
+      <div className='flex  items-center justify-center hover:cursor-pointer hover:bg-white hover:text-black text-white bg-black border-2 border-white w-[30px] p-12 m-2 text-lg'>
         {type === "video" ? <VideocamIcon /> : <SportsEsportsIcon />}
       </div>
     </div>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,8 +5,12 @@ export default function Home() {
   return (
     <div className='bg-black h-[100vh]'>
       <div className='flex w-full h-full items-center justify-center'>
-        <Tab type={"video"} />
-        <Tab type={"game"} />
+        <a href='https://videocallcn.herokuapp.com'>
+          <Tab type={"video"} />
+        </a>
+        <a href='https://fun-time-app.herokuapp.com/'>
+          <Tab type={"game"} />
+        </a>
       </div>
     </div>
   );
